8767fdcf45ccf4f3b67eaca111499c98ace3fb95
ruimpmestre <ruimpmestre@gmail.com>
2016-03-03 17:30:56 +0000

    Update quick-launch.sh

--- quick-launch.sh
+++ quick-launch.sh
@@ -1,7 +1,6 @@
-
 tar -xzvf apache-tomcat*.tar.gz
 cd WarwickAnalyticsTest
 ant
-cp WarwickAnalyticsTest.war ../apache-tomcat-7.0.68/webapps/
-cd ../apache-tomcat-*
+cp WarwickAnalyticsTest.war ../apache-tomcat*/webapps/
+cd ../apache-tomcat*
 bin/startup.sh
